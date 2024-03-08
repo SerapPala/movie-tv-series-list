c1943d195b53ba55b83ad14fd8ec75c8036aafad
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2024-03-07 18:16:57 -0800

    Update MovieDetail.tsx

--- components/MovieDetail.tsx
+++ components/MovieDetail.tsx
@@ -25,13 +25,13 @@ const MovieDetail = ({data}) => {
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                 },
             },
             {
                 breakpoint: 540,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 2,
                 },
             },
         ],
@@ -39,63 +39,89 @@ const MovieDetail = ({data}) => {
 
     //#endregion
 
+    //#region functions
+
+    const showStars = (voteAverage: number) => {
+        const roundedRating = Math.round(voteAverage);
+        const remainder = voteAverage - roundedRating;
+        let stars = '';
+
+        for (let i = 1; i <= roundedRating; i++) {
+            stars += '★';
+        }
+
+        if (remainder >= 0.5) {
+            stars += '½';
+        }
+
+        const totalStars = Math.floor(voteAverage);
+        for (let i = totalStars + 1; i <= 10; i++) {
+            stars += '☆';
+        }
+
+        return stars;
+    }
+
+    //#endregion
+
     return (
         <section className="detail-section">
-            <div className="content-container">
-                <div className="top-content relative">
-                    {data?.backdrop_path ? (
-                            <img style={{width: "100%"}} className={"movie-card__body__image"}
-                                 alt='poster'
-                                 src={`https://image.tmdb.org/t/p/w400/${data?.backdrop_path}`}
-                            />
-                        ) :
-
-                        <div className={"no-image-poster"}>
-                            <div
-                                className={"not-data"}> Not yet available..
-                            </div>
-                        </div>
-                    }
-                </div>
-                <div>
-                <ul className="breadcrumb">
-                        <li><a href="/">Home</a></li>
-                        &nbsp; / &nbsp;
-                        <li className={"breadcrumb__active"}> {data?.original_title}</li>
-                    </ul>
-                    <h1 className="title-lg-bold">
-                        {data?.original_title}
-                    </h1>
-                    <ul className={"detail__genres"}>
-                        {data?.genres?.map((genre: {
-                            id: React.Key | null | undefined;
-                            name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined;
-                        }) => (
-                            <li key={genre.id} className={"detail__genres__genre"}>
-                                {genre.name}
-                            </li>
-                        ))
-                        }
-                    </ul>
-                </div>
-                <div className="detail">
-                    <div>
+            <div className="top-content relative">
+                {data?.poster_path ? (
+                    <img
+                        style={{width: "100%"}}
+                        className="detail-section__image"
+                        alt="poster"
+                        src={`https://image.tmdb.org/t/p/w400/${data?.poster_path}`}
+                    />
+                ) : (
+                    <div className="no-image-poster">
+                        <div className="not-data">Not yet available poster..</div>
+                    </div>
+                )}
+                <div className="overlay"></div>
+                <div className={"overlay-2"}>
+                    <div className="content-container detail-section__center">
                         <div>
-                            {data?.overview}
-                        </div>
-
-                        <div className="read-more-link">
-                            <div>
+                            <ul className="breadcrumb">
+                                <li><a href="/">Home</a></li>
+                                &nbsp; / &nbsp;
+                                <li className={"breadcrumb__active"}> {data?.original_title}</li>
+                            </ul>
+                            <h1 className="title-lg-bold">
+                                {data?.original_title}
+                            </h1>
+                            <div className="read-more-link">
                                 {data?.release_date}
                             </div>
+                            <div className={"overview"}>
+                                {data?.overview}
+                            </div>
+                            <div className="stars">
+                                <div>{showStars(data?.vote_average)}</div>
+                            </div>
+                            {data?.genres &&
+                                <ul className={"detail__genres"}>
+                                    {data?.genres?.map((genre: {
+                                        id: React.Key | null | undefined;
+                                        name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined;
+                                    }) => (
+                                        <li key={genre.id} className={"detail__genres__genre"}>
+                                            {genre.name}
+                                        </li>
+                                    ))
+                                    }
+                                </ul>
+                            }
                         </div>
                     </div>
-
                 </div>
+            </div>
+            <div className="content-container detail-section__center">
                 <div className={"read-more"}>
-                    <h2>
+                    {data?.production_companies?.length >= 1 && <h2>
                         Production Companies
-                    </h2>
+                    </h2>}
                     {data?.production_companies?.length > 3 &&
                         <ul className={"read-more-list-ul"}>
                             <Slider {...SliderSettings}>
@@ -127,10 +153,7 @@ const MovieDetail = ({data}) => {
                             <div
                                 className={"not-data"}>  {data.production_companies.length <= 0 && " Not yet available.."}</div>
                         </ul>
-
-
                     }
-
                     {data?.production_companies?.length <= 3 &&
                         <ul className={"read-more-list-ul company-list"}>
                             {data.production_companies.map((company: {
@@ -159,8 +182,6 @@ const MovieDetail = ({data}) => {
                             }
                         </ul>
                     }
-
-
                 </div>
             </div>
         </section>
